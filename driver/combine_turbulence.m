d31ebf1ba1046c36d5bfa490d420422b8784c533
deepak <dpak.cherian@gmail.com>
2017-12-12 13:43:40 -0800

    516 params.

--- driver/combine_turbulence.m
+++ driver/combine_turbulence.m
@@ -41,7 +41,7 @@ addpath(genpath('./chipod_gust/software/'));% include  path to preocessing routi
       %CP.T2death           = datenum(2000, 1, 1, 0, 0, 0);
       %CP.adcpdeath         = datenum(2000, 1, 1, 0, 0, 0);
       %CP.nantimes{1}       = []; % sensor T1 for chipod or T sensor on gusT
-      %CP.nantimes{2}       = []; % sensor T2 for chipod
+      CP.nantimes{2} = [datenum(2014, 1, 19), datenum(2014, 1, 24)]; % sensor T2 for chipod
       %CP.nantimes{3}       = []; % pitot sensor
       %CP.avgwindow         = 600;
       %CP.depth             = 0;
