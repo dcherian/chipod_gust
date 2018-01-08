ffed4c348cd977848b7cbfe97847534193fb336d
deepak <dpak.cherian@gmail.com>
2018-01-08 10:48:38 -0800

    Use self-contained calibration.

--- driver/calibrate_pitot.m
+++ driver/calibrate_pitot.m
@@ -13,8 +13,8 @@ close all;
    do_raw_data = 0;     % do the averaging of the raw-data (1) or skip (0) if done before
    do_v0_self  = 1;     % detremine V0 based on a min of the averaged signal (self contained)
    do_v0_adcp  = 1;     % detremin V0 based on a fit against reference velocity (adcp) data
-   do_plot     = 0;     % generate some figures in ../pics/ to compare the different velocity estimates
-   do_vel_p    = 1;     % which calibration should be used for vel_p (0 none (default), 1: adcp, 2: self)
+   do_plot     = 1;     % generate some figures in ../pics/ to compare the different velocity estimates
+   do_vel_p    = 2;     % which calibration should be used for vel_p (0 none (default), 1: adcp, 2: self)
 
 %_____________________include path of processing flies______________________
 addpath(genpath('./chipod_gust/software/'));% include  path to preocessing routines
