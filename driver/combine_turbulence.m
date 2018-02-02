008cd071648a3a26ae08678a68a8c81df9229b5b
deepak <dpak.cherian@gmail.com>
2018-02-02 12:03:47 -0800

    Use 1e-3.

--- driver/combine_turbulence.m
+++ driver/combine_turbulence.m
@@ -22,7 +22,7 @@ addpath(genpath('./chipod_gust/software/'));% include  path to preocessing routi
 
    % set thresholds for masking
    min_N2 = 1e-6;
-   min_dTdz = 3e-3;
+   min_dTdz = 1e-3;
    min_spd = 0.05;
    min_inst_spd = min_spd; % min instantaneous speed past sensor
    mask_inst_spd = 1; % estimates are crappy if sensor isn't moving
